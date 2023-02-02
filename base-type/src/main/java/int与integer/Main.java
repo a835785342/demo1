84af598430905e7da46dd8de42c687c37aebb7e7
wxzhangyuanhui <wxzhangyuanhui@gfpartner.com.cn>
2023-02-02 15:25:46 +0800

    基础数据类型-int与integer使用

---
+++ base-type/src/main/java/int与integer/Main.java
@@ -0,0 +1,11 @@
+package int与integer;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer i1 = new Integer(129);
+        Integer i2 = 300;
+        int i3 = 300;
+        System.out.println(i3==i2);
+    }
+
+}
