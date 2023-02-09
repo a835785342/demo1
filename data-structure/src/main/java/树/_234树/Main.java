da1b1fb2a8f7da06fbd6cd84928c5bc8a2c5e74c
wxzhangyuanhui <wxzhangyuanhui@gfpartner.com.cn>
2023-02-09 19:24:19 +0800

    数据结构-2-3-4树

---
+++ data-structure/src/main/java/树/_234树/Main.java
@@ -0,0 +1,14 @@
+package 树._234树;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree234 tree234 = new Tree234();
+        tree234.insert(230);
+        tree234.insert(128);
+        tree234.insert(127);
+        tree234.insert(126);
+        tree234.insert(125);
+        tree234.insert(124);
+        tree234.insert(111);
+    }
+}
