4a1f3ec86f6eb843f9d5d7a70c420b8218289de5
wxzhangyuanhui <wxzhangyuanhui@gfpartner.com.cn>
2023-02-16 16:09:47 +0800

    线程相关-volatile使用

---
+++ thread/src/main/java/volatile使用/TestVolatile.java
@@ -0,0 +1,46 @@
+package volatile使用;
+
+public class TestVolatile {
+
+    public static void main(String[] args) {
+
+        ThreadDemo td = new ThreadDemo();
+        new Thread(td, "ThreadDemo").start();
+
+        while (true) {
+            // 加上下面三句代码的任意一句，程序都会正常结束：
+             System.out.println("!!");                              //...语句1
+//             synchronized (TestVolatile.class) {}                     //...语句2
+//            TestVolatile.test2();                                    //...语句3
+
+            // 若只加上下面一句代码，程序都会死循环：
+            // TestVolite.test1();                                  //...语句4
+
+            if (td.flag) {
+                System.out.println("线程 " + Thread.currentThread().getName()
+                        + " 即将跳出while循环体... ");
+                break;
+            }
+        }
+    }
+
+    public static void test1() {}
+
+    public synchronized static void test2() {}
+}
+
+class ThreadDemo implements Runnable {
+
+    public boolean flag = false;
+
+    @Override
+    public void run() {
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//        }
+        flag = true;
+        System.out.println("线程 " + Thread.currentThread().getName() + " 执行完毕： "
+                + "置  flag= " + flag + " ...");
+    }
+}
