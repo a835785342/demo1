76a0afcb93b46e4a9fce9ca6bc26b528ec643695
wxzhangyuanhui <wxzhangyuanhui@gfpartner.com.cn>
2023-03-07 09:25:36 +0800

    io操作-buffered流使用

--- IO/src/main/java/bufferedInputStream/Main.java
+++ IO/src/main/java/bufferedInputStream/Main.java
@@ -1,33 +1,42 @@
 package bufferedInputStream;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        try (InputStream ai = new ByteArrayInputStream("1234567890".getBytes());
-             InputStream bis = new BufferedInputStream(ai, 4)) {
+//        try (FileInputStream fis = new FileInputStream("/Users/jiangyoushengcai/Downloads/WPS_Office_5.2.1(7798)_universal.dmg");
+//             BufferedInputStream bis = new BufferedInputStream(new FileInputStream("/Users/jiangyoushengcai/Downloads/WPS_Office_5.2.1(7798)_universal.dmg"));
+//             FileOutputStream fos = new FileOutputStream("/Users/jiangyoushengcai/Downloads/WPS_Office_5.2.1(7798)_universal.dmg.fos");
+//             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("/Users/jiangyoushengcai/Downloads/WPS_Office_5.2.1(7798)_universal.dmg.bos"))) {
+//
+//            long start = System.currentTimeMillis();
+//            int data;
+//            byte[] bytes1 = new byte[8192];
+//            while ((bis.read(bytes1,0,bytes1.length)) != -1) {
+//                bos.write(bytes1,0,bytes1.length);
+//            }
+//            System.out.println("缓存流耗时："+(System.currentTimeMillis() - start));
+//
+//            start = System.currentTimeMillis();
+//            byte[] bytes = new byte[8192];
+//            while ((data = fis.read(bytes)) != -1) {
+//                fos.write(bytes,0,data);
+//            }
+//            System.out.println("字节流耗时："+(System.currentTimeMillis() - start));
+//
+//
+//
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-            System.out.println("Char : " + (char) bis.read());    //step:1
-            System.out.println("Char : " + (char) bis.read());    //step:2
-
-            bis.mark(3);                                          //step:3
-            System.out.println("-------mark(3)----------");
-
-            System.out.println("Char : " + (char) bis.read());    //step:4
-            System.out.println("Char : " + (char) bis.read());    //step:5
-            System.out.println("Char : " + (char) bis.read());    //step:6
-            System.out.println("Char : " + (char) bis.read());    //step:6
-
-            bis.reset();                                          //step:7
-            System.out.println("-------reset()----------");
-
-            int b;
-            while ((b = bis.read()) != -1) {
-                System.out.println("char : " + (char) b);
+        try (ByteArrayInputStream bais = new ByteArrayInputStream("1234567".getBytes())) {
+            byte[] bytes2 = new byte[2];
+            int data;
+            while ((data = bais.read(bytes2)) != -1) {
+                System.out.println(new String(bytes2));
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
