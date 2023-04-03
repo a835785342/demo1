83ef6c8f1e95009f88c7d41ef2ef2fd310f8d712
jiangyoushengcai <835785342@qq.com>
2023-04-03 10:18:20 +0800

    springweb应用-tomcat线程测试

--- springweb/src/main/java/webapp/controller/TestController.java
+++ springweb/src/main/java/webapp/controller/TestController.java
@@ -4,15 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @RestController
 public class TestController {
 
     @GetMapping(value = "/test")
     public String test(HttpServletRequest request) throws Exception {
-        System.out.println(Thread.currentThread().getName() + "开始" + request.getRemotePort());
+        System.out.println(Thread.currentThread().getName() + "开始" + request.getRemotePort() + " 时间:" + LocalDateTime.now());
         Thread.sleep(10000);
-        System.out.println(Thread.currentThread().getName() + "结束" + request.getRemotePort());
+        System.out.println(Thread.currentThread().getName() + "结束" + request.getRemotePort() + " 时间:" + LocalDateTime.now());
         return "ok";
     }
 }
