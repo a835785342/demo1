7a55a63917d15044e1daef416f4b87d45d4c7d45
jiangyoushengcai <835785342@qq.com>
2023-10-19 10:35:02 +0800

    脚本自动提交

--- autoPushCode.sh
+++ autoPushCode.sh
@@ -1,6 +1,6 @@
 cd /Users/jiangyoushengcai/Library/CloudStorage/OneDrive-个人/demo1
 git config --global http.proxy http://127.0.0.1:7890
 git add .
-git commit -m "6点自动提交"
+git commit -m "脚本自动提交"
 git push origin main
 git config --global --unset http.proxy
